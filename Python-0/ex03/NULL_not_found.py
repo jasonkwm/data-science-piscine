a1d167e4cccc6842f4d7d6fea87dd66b23815374
Jason Koh <jasonkwm13@gmail.com>
2023-09-18 14:53:31 +0800

    feat: ex03

---
+++ Python-0/ex03/NULL_not_found.py
@@ -0,0 +1,17 @@
+def NULL_not_found(object):
+    isNan = float("nan")
+    # print(object != object)
+    if (object == None):
+        print("Nothing: {} {}".format(object, type(object)))
+    elif (type(isNan) == type(object) and object != object):
+        print("Cheese: {} {}".format(object, type(object)))
+    elif (object == 0 and type(object) == type(int())):
+        print("Zero: {} {}".format(object, type(object)))
+    elif (object == "" and type(object) == type(str())):
+        print("Empty: {} {}".format(object, type(object)))
+    elif (object == False):
+        print("Fake: {} {}".format(object, type(object)))
+    else:
+        print("Type not Found")
+        return 1
+    return 42
