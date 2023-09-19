7e7e79ed0343b0dc67f4115ac6d6d93a1d5260b6
Jason <jasonkwm13@gmail.com>
2023-09-19 11:04:04 +0800

    feat: ex04 norm

--- Python-0/ex04/whatis.py
+++ Python-0/ex04/whatis.py
@@ -1,11 +1,11 @@
 import sys
 
 try:
-   assert len(sys.argv) < 3 , "more than one argument is provided"
-   print(int(sys.argv[1]))
+    assert len(sys.argv) < 3, "more than one argument is provided"
+    print(int(sys.argv[1]))
 except AssertionError as msg:
-   print("AssertionError: {}".format(msg))
+    print("AssertionError: {}".format(msg))
 except ValueError: 
-  print("AssertionError: argument is not an integer")
+    print("AssertionError: argument is not an integer")
 except IndexError:
-   pass
+    pass
