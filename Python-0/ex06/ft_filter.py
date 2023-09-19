715cf6c81b40d42b2ba43150f22723a0f96e0406
Jason <jasonkwm13@gmail.com>
2023-09-19 11:18:06 +0800

    feat: done ft_filter

---
+++ Python-0/ex06/ft_filter.py
@@ -0,0 +1,20 @@
+
+def ft_filter(func, iterable):
+    """
+    filter(function or None, iterable) --> filter object
+
+    Return an iterator yielding those items of iterable for which function(item)
+    is true. If function is None, return the items that are true.
+    """
+    if (func is None):
+        return iterable
+    return [i for i in iterable  if func(i)]
+
+
+# # To Test
+# def f(x):
+#     return x > 1
+# a = ft_filter(f, [1, 2 , 3])
+
+# for i in a:
+#     print(i)
