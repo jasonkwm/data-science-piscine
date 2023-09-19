eda70460edd5a7f544e909d235eac487add8e5be
Jason <jasonkwm13@gmail.com>
2023-09-19 13:17:13 +0800

    fix: norm

--- Python-0/ex04/whatis.py
+++ Python-0/ex04/whatis.py
@@ -5,7 +5,7 @@ try:
     print(int(sys.argv[1]))
 except AssertionError as msg:
     print("AssertionError: {}".format(msg))
-except ValueError: 
+except ValueError:
     print("AssertionError: argument is not an integer")
 except IndexError:
     pass
