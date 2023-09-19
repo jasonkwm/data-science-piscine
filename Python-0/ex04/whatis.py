1ac4f63d869c5be4556b1d2924bed475d8fc8656
Jason <jasonkwm13@gmail.com>
2023-09-19 15:46:15 +0800

    feat: done ex04 previously forgotten

--- Python-0/ex04/whatis.py
+++ Python-0/ex04/whatis.py
@@ -2,10 +2,12 @@ import sys
 
 try:
     assert len(sys.argv) < 3, "more than one argument is provided"
-    print(int(sys.argv[1]))
+    print(f"I'm {'Even.' if int(sys.argv[1]) % 2 == 0 else 'Odd.' }")
 except AssertionError as msg:
     print("AssertionError: {}".format(msg))
 except ValueError:
     print("AssertionError: argument is not an integer")
 except IndexError:
+    pass
+except KeyboardInterrupt:
     pass
