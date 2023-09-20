7ea70ec12718b3b50f8602d79634b3619736ea8d
Jason <jasonkwm13@gmail.com>
2023-09-20 11:17:05 +0800

    feat: add files for ex03

---
+++ Python-1/ex03/load_image.py
@@ -0,0 +1,20 @@
+from PIL import Image, UnidentifiedImageError
+import numpy as np
+
+
+def ft_load(path: str) -> np.array:
+    try:
+        assert isinstance(path, str), "path problem"
+        img = Image.open(path)
+        img_array = np.array(img)
+        print(f"The shape of image is: {img_array.shape}")
+        return img_array
+    except AssertionError as msg:
+        print("AssertionError: {}".format(msg))
+        return np.array([])
+    except FileNotFoundError:
+        print("Error: file not found")
+        return np.array([])
+    except UnidentifiedImageError:
+        print("Error: file not image")
+        return np.array([])
