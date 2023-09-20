a0537aac5bccb8e072125a568e3eed66121d2ba9
Jason <jasonkwm13@gmail.com>
2023-09-20 10:32:17 +0800

    feat: ex00 error checking and add tester

--- Python-1/ex00/give_bmi.py
+++ Python-1/ex00/give_bmi.py
@@ -1,12 +1,22 @@
 import numpy as np
 
-def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
-    if (len(height) != len(weight)):
-        raise ValueError("invalid lists")
-    
-    return np.divide(weight, np.multiply(height, height)).tolist()
 
+def give_bmi(height: list[int | float], weight: list[int | float]):
+    try:
+        assert len(height) == len(weight), "wrong list length"
+        assert np.issubdtype(np.array(weight).dtype, np.number), "wrong type"
+        assert np.issubdtype(np.array(height).dtype, np.number), "wrong type"
+        return np.divide(weight, np.multiply(height, height)).tolist()
+    except AssertionError as msg:
+        print("AssertionError: {}".format(msg))
+        return []
 
 
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
-    return [ i < limit for i in bmi]
+    try:
+        assert np.issubdtype(np.array(bmi).dtype, np.number), "wrong type"
+        assert isinstance(limit, int), "wrong type"
+        return [i < limit for i in bmi]
+    except AssertionError as msg:
+        print("AssertionError: {}".format(msg))
+        return []
