13c80301b3e8c5f8eca70098775bdfbc8f6964e2
Jason <jasonkwm13@gmail.com>
2023-09-21 18:07:26 +0800

    chore: create default files for ex02 and ex03

---
+++ Python-2/ex03/load_csv.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+
+def load(path: str) -> pd.DataFrame | None:
+    """
+    loading csv provided
+    """
+    try:
+        assert isinstance(path, str), "path problem"
+        df = pd.read_csv(path, index_col=None)
+        print(f"Loading dataset of dimensions {df.shape}")
+        return df
+    except AssertionError as msg:
+        print("AssertionError: {}".format(msg))
+        return None
+    except FileNotFoundError:
+        print("Error: file not found")
+        return None
+    except PermissionError:
+        print("Error: file problem")
+        return None
+    except UnicodeDecodeError:
+        print("Error: wrong file")
